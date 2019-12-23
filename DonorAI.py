60716777cf33e4bb48ecd0e46bcb8b1d451154fd
Taylor Lee Schaffner <tschaffner@Taylors-MacBook-Pro.local>
2019-12-23 11:55:43 -0600

    Created a few basic unit tests, finished parsing out csv data, and added a csv sample list for testsing

--- DonorAI.py
+++ DonorAI.py
@@ -1,1 +1,61 @@
 #Donor AI
+
+#Python imports.
+#Part of the Python base package.
+import csv
+import os
+
+#data variables initialized for later use.
+
+#Hard coded shell dictionary.
+shell = { 'priority': '', 
+            'first': '', 
+            'mid': '',
+            'last': '', 
+            'suf': '', 
+            'address': '',
+            'address_2': '',
+            'city': '',
+            'state': '',
+            'zip_postal': '',
+            'zip4': '',
+            'country_code': '',
+            'home': '',
+            'cell': '',
+            'work': '',
+            'email': '',
+            'notes': '',
+            'occupation': '',
+            'employer': ''
+            }
+
+def main():
+    #List all files in the cwd.
+    os.listdir('.')
+    #Empty dict that will contain subdicts representing each row.
+    data_set = parse_csv('example.csv')
+    print(data_set)
+
+#Method for parsing our csv files given a file name.
+def parse_csv(file_name):
+    temp_data = {}
+    #initialize csv file for reading.
+    csv_file = open(file_name)
+    readCSV = csv.reader(csv_file, delimiter=',')
+
+    #Iterate through the data set starting at the second row.
+    row_num = 2
+    next(readCSV)
+    for row in readCSV:
+        #Create a new shell to fill.
+        new_shell = dict(shell)
+        #Fill the new shell.
+        for col, key in zip(row, shell.keys()):
+            new_shell[key] = col
+        temp_data[row_num] = new_shell
+        row_num += 1
+    csv_file.close()
+    return temp_data
+
+if __name__ == '__main__':
+    main()
--- UnitTests.py
+++ UnitTests.py
@@ -1,1 +1,14 @@
 #Unit tests
+import unittest
+import DonorAI as ai
+
+class TestCSVParsing(unittest.TestCase):
+
+    def test_example_file_one(self):
+        data_set = ai.parse_csv('example.csv')
+        self.assertEqual(data_set[2]['priority'], '1/2/2014')
+        self.assertEqual(data_set[3]['priority'], '1/3/2014')
+        self.assertEqual(data_set[4]['priority'], '1/4/2014')
+
+if __name__ == '__main__':
+    unittest.main()
